c7dc68928b4d7a13585d91b1bd73463d7f7f85c3
Lincy <lincgr@gmail.com>
2024-05-30 17:07:05 -0600

    02_02 Tipos de datos String y caracteres en Julia

---
+++ 02_02/app.jl
@@ -0,0 +1,13 @@
+string1 = "¡Hola, mundo!"
+println("Valor de string1: ", string1)
+println("Tipo de string1: ",typeof(string1)) 
+
+lenguaje = "Julia"
+texto = "Aplicación en $lenguaje"
+println(texto)
+
+println(length(texto))
+
+char1 = 'a'
+println("Valor de char1: ", char1)
+println("Tipo de char1: ",typeof(char1))
