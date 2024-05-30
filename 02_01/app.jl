e8a0121e7a9526a847c7bb3d222b0a9f4a901842
Lincy <lincgr@gmail.com>
2024-05-30 16:03:38 -0600

    02_01 Tipos de datos numéricos enteros y flotantes en Julia

---
+++ 02_01/app.jl
@@ -0,0 +1,19 @@
+a = 42 
+println("Valor de a: ", a)
+println("Tipo de a: ", typeof(a))
+
+b = 0x124
+println("Valor de b: ", b)
+println("Tipo de b: ",typeof(b)) 
+
+c = 3.0
+println("Valor de c: ", c)
+println("Tipo de c: ",typeof(c)) 
+
+d = .9
+println("Valor de d: ", d)
+println("Tipo de d: ",typeof(d)) 
+
+e = -3.14
+println("Valor de e: ", e)
+println("Tipo de e: ",typeof(e)) 
