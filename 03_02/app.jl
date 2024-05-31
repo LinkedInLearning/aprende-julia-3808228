ed3a28c82a4755cecd5da96e81909adc6714695b
Lincy <lincgr@gmail.com>
2024-05-31 16:00:55 -0600

    03_02 Operadores de comparación en Julia

---
+++ 03_02/app.jl
@@ -0,0 +1,20 @@
+a = 15
+b = 6
+
+println("Operador de Igualdad")
+println(a == b)
+
+println("Operador de Desigualdad")
+println(a != b)
+
+println("Operador de Mayor que")
+println(a > b)
+
+println("Operador de Mayor o igual que")
+println(a >= b)
+
+println("Operador de Menor que")
+println(a < b)
+
+println("Operador de Menor o igual que")
+println(a <= b)
