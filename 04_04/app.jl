2bd24fb3c9c43e11034b2b0624d69e74e98219d0
Lincy <lincgr@gmail.com>
2024-06-03 09:54:47 -0600

    04_04 Tuplas en Julia

---
+++ 04_04/app.jl
@@ -0,0 +1,12 @@
+tupla = (1, 9, 90.0, 41, 51.0, 63.0, 7.0)
+
+println("Valores de la tupla")
+println(tupla)
+
+println("Imprimir un elemento de la tupla")
+println(tupla[2])
+
+println("Iteración sobre la tupla")
+for elemento in tupla
+    println("Elemento = $elemento")
+end
