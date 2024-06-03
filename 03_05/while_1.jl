977f1cd0d3d41ae5021a916ec04ee7098fda8ab5
Lincy <lincgr@gmail.com>
2024-06-02 19:09:34 -0600

    03_05 Estructura de repetición While en Julia

---
+++ 03_05/while_1.jl
@@ -0,0 +1,6 @@
+contador = 1
+
+while contador <= 100
+    println("Contador: $contador")
+    global contador += 1
+end
---
+++ 03_05/while_2.jl
@@ -0,0 +1,9 @@
+suma = 0
+contador = 1
+
+while contador <= 5
+    global suma += contador
+    global contador += 1
+end
+
+println("La suma del 1 al 5 es : $suma")
