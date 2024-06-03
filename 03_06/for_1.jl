0262ac14ec6f24d2f7c619ac86cff63448c37b56
Lincy <lincgr@gmail.com>
2024-06-02 19:59:42 -0600

    03_06 Estructura de repetición for en Julia

---
+++ 03_06/for_1.jl
@@ -0,0 +1,3 @@
+for i in 1:5
+    println("Contador = $i")
+end
---
+++ 03_06/for_2.jl
@@ -0,0 +1,7 @@
+for i in 1:3
+    for j in 1:10
+        resultado_multiplicacion = i * j
+        println("$i * $j = $resultado_multiplicacion")
+    end
+    println()
+end
